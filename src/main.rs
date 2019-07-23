aaa7baa2fa3068a21fe3edec060a4aa8a7ce6728
Dulhan <dhjayalath@gmail.com>
2019-07-23 19:20:29 +0100

    Useful comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 #[macro_use]
-extern crate lazy_static;
-#[macro_use]
 extern crate text_io;
 extern crate metaflac;
 extern crate rodio;
@@ -21,31 +19,38 @@ mod playlist;
 mod song;
 
 fn main() {
-
+    // TEMPORARY CMD-LINE ARGS BEFORE GUI
+    // Read cmd-line arguments and check errors
     let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
+    if args.len() <= 2 {
         eprintln!("ERROR: Too few args");
         process::exit(1);
     }
-
     let dir = WalkDir::new(&args[1]);
     let valid_exts = args[2].split(',').collect::<Vec<&str>>();
 
+    // Create playlist from songs located in directory root
     let songs = find_music(dir, &valid_exts).unwrap();
     let mut playlist = Playlist::new(songs);
 
-    playlist.random_shuffle();
-
+    // Thread send/receive channels to communicate cmd-line
+    // commands to control playlist
     let (tx, rx) = mpsc::channel();
 
+    playlist.random_shuffle();
+
+    // Cmd-line command collection thread
     thread::spawn(move || loop {
         let cmd: String = read!();
         tx.send(cmd).unwrap();
     });
 
+    // Playlist runs on main thread
     loop {
         playlist.update();
 
+        // Ensure main thread doesn't wait for new
+        // commands if none are provided
         if let Ok(c) = &rx.try_recv() {
             match &c[..] {
                 "skip" => playlist.play_next(),
@@ -63,8 +68,12 @@ fn find_music(path: WalkDir, valid_exts: &[&str]) -> Result<Vec<Song>, Box<dyn E
         let entry = entry?;
         let loc = entry.clone();
         let entry = entry.path();
+
+        // Check extension exists!
         if let Some(extension) = entry.extension() {
+            // Make certain it is an extension that can be played
             if valid_exts.contains(&extension.to_str().unwrap()) {
+                // Find relevant Song attributes
                 let file = std::fs::File::open(entry)?;
                 let reader = BufReader::new(file);
                 let source = rodio::Decoder::new(reader)?;
@@ -75,17 +84,29 @@ fn find_music(path: WalkDir, valid_exts: &[&str]) -> Result<Vec<Song>, Box<dyn E
 
                 match metaflac::Tag::read_from_path(entry) {
                     Ok(tag) => {
+                        // The genre tag requires more work by splitting strings
+                        // using certain characters because genre tagging is
+                        // inconsistent. Some tags are written as a single string
+                        // with delimiters. Others are done properly as separate
+                        // strings in vorbis.
                         if let Some(g) = tag.get_vorbis("genre") {
                             for genre_field in g {
+                                // Note this is not ideal because the standard tag 'Hip Hop', if
+                                // tagged as 'Hip-Hop' is split into 'Hip' and 'Hop'
                                 for genre_type in genre_field.split(&[',', '/', '\\', ';', '-'][..])
                                 {
                                     genre.push(genre_type.to_string());
                                 }
                             }
                         }
+
+                        // Title and artist tags are just one string so
+                        // only the first index of the vector is needed
+
                         if let Some(t) = tag.get_vorbis("title") {
                             title = t[0].clone();
                         }
+
                         if let Some(a) = tag.get_vorbis("artist") {
                             artist = a[0].clone();
                         }
--- src/playlist.rs
+++ src/playlist.rs
@@ -25,7 +25,7 @@ impl Playlist {
     }
 
     pub fn play_next(&mut self) {
-        self.songs.remove(0);
+        self.songs.remove(0); // FIX skipping first song
         self.stop_sink();
         self.songs[0].play(&self.sink).unwrap();
         self.reset_time();
@@ -37,19 +37,21 @@ impl Playlist {
         self.play_next();
     }
 
+    // A function that shuffles non-randomly to try and chain songs
+    // with the most matching genres
     pub fn genre_shuffle(&mut self) {
-
         self.stop_sink();
 
+        // Loop over every song currently after now playing
         for k in 1..self.songs.len() {
-
+            // Track the index of the song with the most
+            // genre matches to the song being analysed
             let mut best_score = 0;
             let mut best_i = k;
 
             for i in k..self.songs.len() {
                 let mut score = 0;
                 for genre in &self.songs[k - 1].genre {
-
                     if self.songs[i].genre.contains(&genre) {
                         score += 1;
                     }
@@ -59,12 +61,12 @@ impl Playlist {
                     best_score = score;
                     best_i = i;
                 }
-
             }
 
+            // Insert the most similar song into the spot
+            // after the song currently being analysed
             let temp = self.songs.remove(best_i);
             self.songs.insert(k, temp);
-
         }
 
         self.play_next();
@@ -80,9 +82,12 @@ impl Playlist {
         self.start = SystemTime::now();
     }
 
+    // Check if enough time has passed that the
+    // playing song has finished
     fn is_song_finished(&self) -> bool {
         let now = SystemTime::now();
-        if now.duration_since(self.start).unwrap().as_millis() > self.songs[0].duration.as_millis() {
+        if now.duration_since(self.start).unwrap().as_millis() > self.songs[0].duration.as_millis()
+        {
             return true;
         }
 
