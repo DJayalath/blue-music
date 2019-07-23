6f9dfb723a095f1f97be72b538e02dc98c0b2eed
Dulhan <dhjayalath@gmail.com>
2019-07-23 18:59:24 +0100

    Added genre consistent shuffling

--- src/playlist.rs
+++ src/playlist.rs
@@ -37,6 +37,39 @@ impl Playlist {
         self.play_next();
     }
 
+    pub fn genre_shuffle(&mut self) {
+
+        self.stop_sink();
+
+        for k in 1..self.songs.len() {
+
+            let mut best_score = 0;
+            let mut best_i = k;
+
+            for i in k..self.songs.len() {
+                let mut score = 0;
+                for genre in &self.songs[k - 1].genre {
+
+                    if self.songs[i].genre.contains(&genre) {
+                        score += 1;
+                    }
+                }
+
+                if score > best_score {
+                    best_score = score;
+                    best_i = i;
+                }
+
+            }
+
+            let temp = self.songs.remove(best_i);
+            self.songs.insert(k, temp);
+
+        }
+
+        self.play_next();
+    }
+
     pub fn update(&mut self) {
         if self.is_song_finished() {
             self.play_next();
@@ -49,7 +82,7 @@ impl Playlist {
 
     fn is_song_finished(&self) -> bool {
         let now = SystemTime::now();
-        if now.duration_since(self.start).unwrap().as_secs() > self.songs[0].duration.as_secs() {
+        if now.duration_since(self.start).unwrap().as_millis() > self.songs[0].duration.as_millis() {
             return true;
         }
 
