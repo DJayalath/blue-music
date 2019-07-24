152b63d8cf55ffdf9bb5fa33904df8a1111d70c0
Dulhan <dhjayalath@gmail.com>
2019-07-24 15:49:15 +0100

    Relm interface

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,26 @@ extern crate metaflac;
 extern crate rodio;
 extern crate walkdir;
 
+extern crate gdk_pixbuf;
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+
+use relm::{Relm, Update, Widget};
+use gtk::prelude::*;
+use gtk::Orientation::{Vertical, Horizontal};
+use gtk::Image;
+use gtk::{OrientableExt, ToolButtonExt};
+use gtk::{GtkWindowExt, Inhibit, WidgetExt};
+use gdk_pixbuf::Pixbuf;
+use gtk::{Adjustment, BoxExt, ImageExt, LabelExt, ScaleExt};
+use relm_derive::widget;
+
+pub const PAUSE_ICON: &str = "gtk-media-pause";
+pub const PLAY_ICON: &str = "gtk-media-play";
+
 use playlist::Playlist;
 use rodio::Source;
 use song::Song;
@@ -32,6 +52,8 @@ fn main() {
     // Create playlist from songs located in directory root
     let songs = find_music(dir, &valid_exts).unwrap();
     let mut playlist = Playlist::new(songs);
+
+    Win::run(()).unwrap();
 
     // Thread send/receive channels to communicate cmd-line
     // commands to control playlist
@@ -121,4 +143,149 @@ fn find_music(path: WalkDir, valid_exts: &[&str]) -> Result<Vec<Song>, Box<dyn E
     }
 
     Ok(songs)
+}
+
+
+#[derive(Msg)]
+pub enum Msg {
+    Open,
+    PlayPause,
+    Previous,
+    Stop,
+    Next,
+    Remove,
+    Save,
+    Quit,
+}
+
+pub struct Model {
+    adjustment: Adjustment,
+    cover_pixbuf: Option<Pixbuf>,
+    cover_visible: bool,
+    current_duration: u64,
+    current_time: u64,
+    play_image: Image,
+}
+
+#[widget]
+impl Widget for Win {
+    fn model() -> Model {
+        Model {
+            adjustment: Adjustment::new(
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+            ),
+            cover_pixbuf: None,
+            cover_visible: false,
+            current_duration: 0,
+            current_time: 0,
+            play_image: new_icon(PLAY_ICON),
+        }
+    }
+
+    fn update(&mut self, event: Msg) {
+        match event {
+            // A call to self.label1.set_text() is automatically inserted by the
+            // attribute every time the model.counter attribute is updated.
+            Msg::Open => (),
+            Msg::PlayPause => (),
+            Msg::Previous => (),
+            Msg::Stop => (),
+            Msg::Next => (),
+            Msg::Remove => (),
+            Msg::Save => (),
+            Msg::Quit => gtk::main_quit(),
+        }
+    }
+
+    view! {
+        gtk::Window {
+            title: "Blue Music",
+            gtk::Box {
+                orientation: Vertical,
+                #[name="toolbar"]
+                gtk::Toolbar {
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("document-open"),
+                        clicked => Msg::Open,
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("document-save"),
+                        clicked => Msg::Save,
+                    },
+                    gtk::SeparatorToolItem {
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("gtk-media-previous"),
+                        clicked => Msg::Previous,
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &self.model.play_image,
+                        clicked => Msg::PlayPause,
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("gtk-media-stop"),
+                        clicked => Msg::Stop,
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("gtk-media-next"),
+                        clicked => Msg::Next,
+                    },
+                    gtk::SeparatorToolItem {
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("remove"),
+                        clicked => Msg::Remove,
+                    },
+                    gtk::SeparatorToolItem {
+                    },
+                    gtk::ToolButton {
+                        icon_widget: &new_icon("gtk-quit"),
+                        clicked => Msg::Quit,
+                    },
+                },
+                gtk::Image {
+                    from_pixbuf: self.model.cover_pixbuf.as_ref(),
+                    visible: self.model.cover_visible,
+                },
+                gtk::Box {
+                    orientation: Horizontal,
+                    spacing: 10,
+                    gtk::Scale(Horizontal, &self.model.adjustment) {
+                        draw_value: false,
+                        hexpand: true,
+                    },
+                    gtk::Label {
+                        text: &millis_to_minutes(self.model.current_time),
+                    },
+                    gtk::Label {
+                        text: "/",
+                    },
+                    gtk::Label {
+                        // TODO: margin_right: 10,
+                        text: &millis_to_minutes(self.model.current_duration),
+                    },
+                }
+            },
+            // Use a tuple when you want to both send a message and return a value to
+            // the GTK+ callback.
+            delete_event(_, _) => (Msg::Quit, Inhibit(false)),
+        }
+    }
+
+    fn init_view(&mut self) {
+        self.toolbar.show_all();
+    }
+}
+
+fn millis_to_minutes(millis: u64) -> String {
+    let mut seconds = millis / 1_000;
+    let minutes = seconds / 60;
+    seconds %= 60;
+    format!("{}:{:02}", minutes, seconds)
+}
+
+fn new_icon(icon: &str) -> Image {
+    Image::new_from_file(
+        format!("./assets/{}.png", icon)
+    )
 }
